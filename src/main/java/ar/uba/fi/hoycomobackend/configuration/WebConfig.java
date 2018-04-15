ba1312b0872ac1138fed9dbcc858efa320f3a32d
Fernando Cancellara <fdcancellara@gmail.com>
2018-04-15 18:47:54 -0300

    Endpoint fix (#5)
    * endpointFix adding fix for endpoints
    
    * Adding swagger to the webmvc config

--- src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
+++ src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
