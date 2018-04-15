c82230fedb4c6b371358c51b24c2db2ee9c9372c
Fernando Cancellara <fdcancellara@gmail.com>
2018-04-15 18:46:56 -0300

    Adding swagger to the webmvc config

--- src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
+++ src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
@@ -1,9 +1,7 @@
 package ar.uba.fi.hoycomobackend.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -11,6 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/*");
+        registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
