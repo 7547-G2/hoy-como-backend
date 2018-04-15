8dec7bbe852bbb1a4c6c98dd302685075b0d9eae
Fernando Cancellara <fdcancellara@gmail.com>
2018-04-15 18:50:17 -0300

    Hotfix missing import

--- src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
+++ src/main/java/ar/uba/fi/hoycomobackend/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package ar.uba.fi.hoycomobackend.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
