816a678a44b49b6ce71de3c2a7faa2c82a7e9b08
Fernando Cancellara <fdcancellara@gmail.com>
2018-06-03 20:25:49 -0300

    #122 Agregar datos del encargado al comercio

--- src/main/java/ar/uba/fi/hoycomobackend/api/dto/ComercioHoyComoAddDto.java
+++ src/main/java/ar/uba/fi/hoycomobackend/api/dto/ComercioHoyComoAddDto.java
@@ -15,6 +15,33 @@ public class ComercioHoyComoAddDto {
     private Double longitud;
     private Integer descuento;
     private String imagenComercio;
+    private String nombreEncargado;
+    private String dniEncargado;
+    private String telefonoEncargado;
+
+    public String getNombreEncargado() {
+        return nombreEncargado;
+    }
+
+    public void setNombreEncargado(String nombreEncargado) {
+        this.nombreEncargado = nombreEncargado;
+    }
+
+    public String getDniEncargado() {
+        return dniEncargado;
+    }
+
+    public void setDniEncargado(String dniEncargado) {
+        this.dniEncargado = dniEncargado;
+    }
+
+    public String getTelefonoEncargado() {
+        return telefonoEncargado;
+    }
+
+    public void setTelefonoEncargado(String telefonoEncargado) {
+        this.telefonoEncargado = telefonoEncargado;
+    }
 
     public Long getId() {
         return id;
