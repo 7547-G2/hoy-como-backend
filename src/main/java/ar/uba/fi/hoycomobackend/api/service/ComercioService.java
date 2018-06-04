7aa288113f038f99350af6439d28096a00d0ffea
Fernando Cancellara <fdcancellara@gmail.com>
2018-06-04 01:22:43 -0300

    fix lead time

--- src/main/java/ar/uba/fi/hoycomobackend/api/service/ComercioService.java
+++ src/main/java/ar/uba/fi/hoycomobackend/api/service/ComercioService.java
@@ -104,9 +104,9 @@ public class ComercioService {
             Pedido pedido = pedidoOptional.get();
             Comercio comercio = comercioQuery.getComercioById(pedido.getStoreId()).get();
             pedido.setEstado(estado);
-            if (PedidoEstado.EN_PREPARACION.equals(estado))
+            if ("EnPreparacion".equalsIgnoreCase(estado))
                 pedido.setStartTime(new Date().getTime());
-            if (PedidoEstado.DESPACHADO.equals(estado)) {
+            if ("Despachado".equalsIgnoreCase(estado)) {
                 pedido.setEndTime(new Date().getTime());
                 Long totalTimeTakenMillis = pedido.getEndTime() - pedido.getStartTime();
                 Integer totalTimeTakenMinutes = (int) Math.ceil(totalTimeTakenMillis.doubleValue() / 60000.0);
